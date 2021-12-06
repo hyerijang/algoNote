85543589551d1b765adc262f3cd2484992afbd1b
Hyeri Jang <chjang5621@naver.com>
2021-12-06 15:30:38 +0900

    build : 테스트 환경설정
    1. jacoco 추가
    - 커버리지 테스트 결과는 build/jacocoReports 에서 확인 가능
    - 브랜치 커버리지 : 80%
    - 라인 커버리지 : 80%
    
    2. 테스트 application.yml 설정
    - h2 인메모리 테스트
    
    Resolves : #17

--- src/test/java/com/jhr/algoNote/AlgoNoteApplicationTests.java
+++ src/test/java/com/jhr/algoNote/AlgoNoteApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AlgoNoteApplicationTests {
+public class AlgoNoteApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
