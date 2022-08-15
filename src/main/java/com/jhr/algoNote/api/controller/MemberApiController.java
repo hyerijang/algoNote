0b76ef293a4a86fe05ade34dfe9a4a695e1e5f2b
Hyeri Jang <chjang5621@naver.com>
2022-08-15 18:02:51 +0900

    feat : 회원 정보 수정 API 구현
    Resolves : #34

--- src/main/java/com/jhr/algoNote/api/controller/MemberApiController.java
+++ src/main/java/com/jhr/algoNote/api/controller/MemberApiController.java
@@ -7,9 +7,14 @@ import com.jhr.algoNote.domain.Member;
 import com.jhr.algoNote.domain.Role;
 import com.jhr.algoNote.service.MemberService;
 import javax.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,5 +47,33 @@ public class MemberApiController {
         return new CreateMemberResponse(member.getId());
     }
 
+    @PatchMapping("/{id}")
+    public updateMemberResponse update(
+        @PathVariable Long id,
+        @RequestBody @Valid UpdateMemberRequest updateMemberRequest) {
+
+        Member findMember = memberService.findOne(id);
+        memberService.update(id, updateMemberRequest.getName(), updateMemberRequest.getPicture());
+        return new updateMemberResponse(id, updateMemberRequest.getName(),
+            updateMemberRequest.getPicture());
+
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+
+        private String name;
+        private String picture;
+
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class updateMemberResponse {
+
+        private Long id;
+        private String name;
+        private String picture;
+    }
 
 }
