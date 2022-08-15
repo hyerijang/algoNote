84b73b99ae6cf3813eb08f1cb77c83227ccbd18c
Hyeri Jang <chjang5621@naver.com>
2022-08-15 18:30:59 +0900

    feat : 회원 조회 API 구현
    Resolves : #34

--- src/main/java/com/jhr/algoNote/api/controller/MemberApiController.java
+++ src/main/java/com/jhr/algoNote/api/controller/MemberApiController.java
@@ -6,15 +6,17 @@ import com.jhr.algoNote.api.dto.CreateMemberResponse;
 import com.jhr.algoNote.domain.Member;
 import com.jhr.algoNote.domain.Role;
 import com.jhr.algoNote.service.MemberService;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -76,4 +78,27 @@ public class MemberApiController {
         private String picture;
     }
 
+    @GetMapping
+    public Result members() {
+        List<Member> members = memberService.findMembers();
+        List<MemberDto> collect = members.stream()
+            .map(m -> new MemberDto(m.getId(), m.getName(), m.getPicture()))
+            .collect(Collectors.toList());
+
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private Long id;
+        private String name;
+        private String picture;
+    }
 }
