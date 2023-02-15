f75aad996808e320d9eb8a96defa259656f944c4
Hyeri Jang <chjang5621@naver.com>
2023-02-15 18:26:58 +0900

    feat: 홈페이지에서 문제 조회 되도록 수정

--- src/main/java/com/jhr/algoNote/controller/HomeController.java
+++ src/main/java/com/jhr/algoNote/controller/HomeController.java
@@ -2,11 +2,18 @@ package com.jhr.algoNote.controller;
 
 import com.jhr.algoNote.config.auth.LoginUser;
 import com.jhr.algoNote.config.auth.dto.SessionUser;
+
 import javax.servlet.http.HttpSession;
+
+import com.jhr.algoNote.domain.Problem;
+import com.jhr.algoNote.repository.query.ProblemSearch;
+import com.jhr.algoNote.service.ProblemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 
 @Controller
@@ -14,6 +21,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     private final HttpSession httpSession;
+    private final ProblemService problemService;
 
     @GetMapping("/")
     public String index(Model model, @LoginUser SessionUser user) {
@@ -21,6 +29,15 @@ public class HomeController {
             model.addAttribute("userName", user.getName());
             model.addAttribute("userImg", user.getPicture());
         }
+
+        //문제 조회
+        ProblemSearch problemSearch = ProblemSearch.builder()
+                .memberEmail(user.getEmail())
+                .build();
+        List<Problem> problems = problemService.search(problemSearch);
+
+        model.addAttribute("problems", problems);
+
         return "home";
     }
 }
